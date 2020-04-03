7c7d74c971ce110c5b46156aec55265f67a5b4cb
Andre <andreseguradopinho@gmail.com>
2020-04-03 17:06:28 +0100

    added proper group.project package structure

--- src/main/java/bellosqueues/spaceexplosions/Asteroid.java
+++ src/main/java/bellosqueues/spaceexplosions/Asteroid.java
@@ -1,3 +1,5 @@
+package bellosqueues.spaceexplosions;
+
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Asteroid {
--- src/main/java/bellosqueues/spaceexplosions/KeyboardController.java
+++ src/main/java/bellosqueues/spaceexplosions/KeyboardController.java
@@ -1,3 +1,5 @@
+package bellosqueues.spaceexplosions;
+
 public class KeyboardController {
 
 
