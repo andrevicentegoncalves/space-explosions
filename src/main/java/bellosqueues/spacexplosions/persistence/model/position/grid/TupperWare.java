55b23863890f95968818a6a1042134b1c6d97916
Andre <andreseguradopinho@gmail.com>
2020-04-05 18:52:37 +0100

    changed Cell to Graphical Position, started GameView in MainMethod

--- src/main/java/bellosqueues/spacexplosions/persistence/model/position/grid/TupperWare.java
+++ src/main/java/bellosqueues/spacexplosions/persistence/model/position/grid/TupperWare.java
@@ -1,21 +1,20 @@
 package bellosqueues.spacexplosions.persistence.model.position.grid;
 
 import bellosqueues.spacexplosions.persistence.model.position.Position;
-import bellosqueues.spacexplosions.persistence.model.position.grid.Cell;
 
 import java.util.HashMap;
 import java.util.Iterator;
 
 public class TupperWare implements Iterable<Position>{
 
-  private HashMap<Position, Cell> hashMap;
+  private HashMap<Position, SimpleGfxPosition> hashMap;
 
   public TupperWare(){
     hashMap = new HashMap<>();
   }
 
   public void addCell(Position position){
-    hashMap.put(position, new Cell(position));
+    hashMap.put(position, new SimpleGfxPosition(position));
   }
 
   public void getCell(Position pos){
--- src/main/java/bellosqueues/spacexplosions/views/GameView.java
+++ src/main/java/bellosqueues/spacexplosions/views/GameView.java
@@ -1,9 +1,20 @@
 package bellosqueues.spacexplosions.views;
 
+import bellosqueues.spacexplosions.persistence.model.Player;
+import bellosqueues.spacexplosions.persistence.model.position.grid.SimpleGfxPosition;
 import bellosqueues.spacexplosions.services.MapService;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class GameView {
+
+    private Picture background = new Picture(10,10, "images/space_arena.png");
+    private Picture player;
+
+    public GameView(Player player){
+        SimpleGfxPosition pos = new SimpleGfxPosition(player.getPosition());
+        this.player = new Picture(pos.getCol(), pos.getRow(), "images/ship.png");
+    }
+
 
     public void show(){
 
@@ -12,11 +23,12 @@ public class GameView {
          background
          player && obstacles
          */
-        Picture background = new Picture(10,10,"src/main/resources/images/space_arena.png");
+
 
         MapService mapService = new MapService(28,8);
         mapService.init();
         background.draw();
+        player.draw();
     }
 
 }
